4071515f84a87911f3a32b59a2de994298b5a85a
Kiril Nedelev <kiril.nedelev98@gmail.com>
2019-05-20 19:13:34 +0300

    init

---
+++ src/main/java/com/fmi/nedelev/NedelevApplication.java
@@ -0,0 +1,13 @@
+package com.fmi.nedelev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NedelevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NedelevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fmi/nedelev/NedelevApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fmi.nedelev;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NedelevApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
