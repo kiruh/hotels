94d203a6bf609918eadb16dde53eddf91cfbc4e4
Kiril Nedelev <kiril.nedelev98@gmail.com>
2019-05-22 00:03:13 +0300

    init webapp

---
+++ webapp/.eslintrc.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const OFF = 0;
+const WARN = 1;
+const ERROR = 2;
+
+module.exports = {
+	parser: "babel-eslint",
+	extends: ["airbnb", "plugin:prettier/recommended"],
+	rules: {
+		"react/jsx-indent": [ERROR, "tab"],
+		"react/jsx-indent-props": [ERROR, "tab"],
+		"linebreak-style": OFF,
+	},
+	settings: {
+		"import/resolver": {
+			webpack: {
+				config: "webpack.config.js",
+			},
+		},
+	},
+	env: {
+		browser: true,
+	},
+};
---
+++ webapp/webpack.config.js
@@ -0,0 +1,92 @@
+const path = require("path");
+const FriendlyErrorsPlugin = require("friendly-errors-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WebpackLivereloadPlugin = require("webpack-livereload-plugin");
+
+const ROOT = path.resolve(__dirname);
+const isProd = process.env.NODE_ENV === "production";
+
+const config = {
+	entry: {
+		app: `./src/index`,
+	},
+	output: {
+		path: path.resolve(ROOT, `../src/main/resources/static/`),
+		filename: "index.js",
+		chunkFilename: "[name].js",
+		publicPath: `/src/main/resources/static/`,
+	},
+	watchOptions: {
+		poll: true,
+	},
+	module: {
+		rules: [
+			/** ******* assets rules ******* */
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				use: "babel-loader",
+			},
+			{
+				test: /\.(css|less)$/,
+				exclude: /node_modules/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: "css-loader",
+						options: {
+							modules: true,
+							importLoaders: 2, // 2 => postcss-loader, less-loader
+							localIdentName: isProd
+								? "[hash:base64:7]"
+								: "[local]___[hash:base64:5]",
+						},
+					},
+					"less-loader",
+				],
+			},
+			/** ******* assets rules ******* */
+
+			/** ******* node_modules rules ******* */
+			{
+				test: /\.css$/,
+				include: /node_modules/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: "css-loader",
+						options: {
+							importLoaders: 1, // 1 => postcss-loader
+						},
+					},
+				],
+			},
+			/** ******* node_modules rules ******* */
+		],
+	},
+	resolve: {
+		alias: {
+			"~": path.resolve(ROOT, "./src"),
+		},
+		extensions: [".js", ".jsx"],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "index.css",
+			chunkFilename: "[name].css",
+		}),
+		new FriendlyErrorsPlugin(),
+		new WebpackLivereloadPlugin({
+			appendScriptTag: true,
+			delay: 1,
+		}),
+	],
+	performance: { hints: false },
+	stats: {
+		// One of the two if I remember right
+		entrypoints: false,
+		children: false,
+	},
+};
+
+module.exports = config;
